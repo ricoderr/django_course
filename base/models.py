9554e080c5c3f7e96dce3783c045b5f4a25d9565
Rijan Gautam <rijangautam07@gmail.com>
2025-04-01 22:13:35 +0545

    update

--- base/models.py
+++ base/models.py
@@ -46,7 +46,7 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1 , choices=PAYMENT_STATUS_CHOICES , default=PAYMENT_STATUS_PENDING)
-    customer = models.Foreignkey(Customer, on_delete = models.PROTECT)
+    customer = models.ForeignKey(Product , on_delete=models.PROTECT)
 
 
 class Cart(models.Model):
